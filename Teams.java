05ab87112cab1e75db26a624ac27c24d28045b22
Madhushree Ganiga <92204525+Madhushree-ganiga@users.noreply.github.com>
2021-10-09 02:28:41 -0700

    Add files via upload

---
+++ Teams.java
@@ -0,0 +1,55 @@
+public class Teams
+{
+  private String teamName;
+  private String captan;
+  private String coach;
+  private String place;
+  private byte noOfPlayers;
+
+  public String getTeamName()
+  {
+	  return teamName;
+  }
+  public void setTeamName(String teamName)
+  {
+	  this.teamName=teamName;
+  }
+
+public String getCaptan()
+  {
+	  return captan;
+  }
+   public void setCaptan(String captan)
+  {
+	  this.captan=captan;
+  }
+
+public String  getCoach()
+  {
+	  return coach;
+  }
+   public void setCoach(String  coach)
+  {
+	  this.coach=coach;
+  }
+  
+  public String  getPlace()
+  {
+	  return place;
+  }
+   public void setPlace(String  place)
+  {
+	  this.place=place;
+  }
+
+public byte getNoOfPlayers()
+  {
+	  return noOfPlayers;
+  }
+   public void setNoOfPlayers(byte noOfPlayers)
+  {
+	  this.noOfPlayers=noOfPlayers;
+  }
+ 
+
+}
